a42356f4edc362f5141963f7c7075c699ffcec60
Nicolas Bürgler <trajan15.nb@gmail.com>
2020-05-19 15:39:41 +0200

    initial

---
+++ ProbeAPI/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProbeAPI.Model;
+
+namespace ProbeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        ProbeAPIContext _context;
+
+        public PersonController(ProbeAPIContext context)
+        {
+            _context = context;
+        }
+        //public string Index()
+        //{
+        //    return "Hans";
+        //}
+
+        public string AddPerson()
+        {
+            var person = new Person();
+            person.Name = "Nicolas";
+            _context.Person.Add(person);
+            _context.SaveChanges();
+            return person.Name;
+        }
+    }
+}
---
+++ ProbeAPI/ProbeAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProbeAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProbeAPI
+{
+    public class ProbeAPIContext : DbContext
+    {
+        public ProbeAPIContext(DbContextOptions<ProbeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
